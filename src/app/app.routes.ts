723ad8fe7ff026a91aba112906838471587d50ac
Josimar Santana <jo.santanna@hotmail.com>
2025-05-06 16:18:08 -0300

    segundo commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
+  {path: 'login', 
+    loadComponent:() => import('./pages/login/login.component').then(c=>c.LoginComponent)
+  },
+  {path: 'home',
+    loadComponent:() => import('./pages/home/home.component').then(c=>c.HomeComponent)
+  },
+  {path: 'dashboard',
+    loadComponent:() => import('./pages/dashboard/dashboard.component').then(c=>c.DashboardComponent)
+  },
   { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent }
+  
 ];
+
