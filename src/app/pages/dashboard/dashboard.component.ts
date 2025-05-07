eb4cfd1c56b557a8525a5da581fac7f639ffd257
Josimar Santana <jo.santanna@hotmail.com>
2025-05-07 16:48:08 -0300

    salvando projeto

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  imports:[CommonModule],
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  vehicles: any[] = [];
+  selectedVehicle: any = null;
+  vehicleData: any[] = [];
+  searchTerm: string = '';
+  dataSearchTerm: string = '';
 
+  constructor(private apiService: ApiService) { }
+  
+  ngOnInit(): void {
+    this.apiService.getVehicles().subscribe(data => {
+      this.vehicles = data;
+    });
+
+    this.loadVehicleData();
+  }
+
+  loadVehicleData(): void {
+    this.apiService.getVehicleData().subscribe(data => {
+      this.vehicleData = data;
+    });
+  }
+
+  onVehicleSelect(vehicle: any): void {
+    this.selectedVehicle = vehicle;
+  }
+
+  get filteredData(): any[] {
+    if (!this.dataSearchTerm) return this.vehicleData;
+    return this.vehicleData.filter(item => 
+      item.codigo.toLowerCase().includes(this.dataSearchTerm.toLowerCase())
+    );
+  }
 }
